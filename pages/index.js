d4d69e28e7b7186ecd36821576aaf010701a5c50
Abby Fischler <abigail.fischler@gmail.com>
2022-01-28 20:20:07 -0800

    Updated text

--- pages/index.js
+++ pages/index.js
@@ -23,23 +23,23 @@ export default function Home() {
               <h1 className="text-5xl font-bold text-red dark:text-white">Why join the club?</h1>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-10 lg:mx-36">
                 <div className="flex flex-col">
-                  <h1 className="dark:text-black">THIS IS A HEADER</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <h1 className="dark:text-black">Become apart of a community</h1>
+                  <p>Hack Club gives you a worldwide community of thousands of other young makers to talk to. We’re artists, writers, engineers, tinkerers, campers, filmmakers, volunteers. We make things. We help one another. We have fun. Join us.</p>
                 </div>
 
                 <div className="flex flex-col">
-                  <h1 className="dark:text-black">THIS IS A HEADER</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <h1 className="dark:text-black">Learn as you build</h1>
+                  <p>Just as the best carpenters didn’t learn in the classroom, neither did the best programmers. Through our workshops, you’ll be walked through building projects. Starting out, you won’t understand how the code works, but you’ll build understanding as you go. You’ll get stuck along the way, but we’re here to help.</p>
                 </div>
 
                 <div className="flex flex-col">
-                  <h1 className="dark:text-black">THIS IS A HEADER</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <h1 className="dark:text-black">Start build</h1>
+                  <p>Most coding classes teach you programming concepts instead of how to write real code—it’s like trying to learn carpentry without any wood. So at Hack Club, you learn to code entirely through building things. You start with no experience and build and ship a project every meeting.</p>
                 </div>
 
                 <div className="flex flex-col">
-                  <h1 className="dark:text-black">THIS IS A HEADER</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <h1 className="dark:text-black">Coding is the future</h1>
+                  <p>Learning to code is uniquely like gaining a superpower: it converts you from a consumer to a creator. Suddenly, computers become a tool for creating.</p>
                 </div>
               </div>
           </section>
